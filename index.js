1296c9fe8d86bd8eae228cf21f28f1acc0893cb7
Albert <albert@skyciv.com>
2022-01-15 19:49:59 +0800

    change the opensource in modules

--- index.js
+++
@@ -1,6 +0,0 @@
-import {ASCE710service} from './codes/ACI318.js'
-// const american = require('./codes/ACI318') //American load combinations
-
-
-
-module.exports = ASCE710service
---
+++ index.mjs
@@ -0,0 +1,6 @@
+import * as ASCE from './codes/ACI318.mjs'
+console.log(ASCE)
+
+// export ASCE
+
+// module.exports = ASCE
