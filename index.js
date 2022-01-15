04bc26c8c0809440e7db1c559c51cfbb2e77570b
Albert <albert@skyciv.com>
2022-01-15 22:22:42 +0800

    working load combinations

--- index.js
+++ index.js
@@ -1,9 +1,4 @@
+const fs = require('fs')
 const ASCE = require('./codes/ACI318.js')
 
-// module.import = * as ASCE from './codes/ACI318.js'
-
 module.exports = ASCE
-
-// export ASCE
-
-// module.exports = ASCE
